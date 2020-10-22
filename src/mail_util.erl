00490fedd11b22b8236ae9d0e86cb432dc1fdee6
Joakim G <joagre@gmail.com>
2020-10-22 19:47:26 +0200

    added a mail footer to unverfied mail and set X-priority mail header to 3. Does only work in Outlook. :-(

--- src/mail_util.erl
+++ src/mail_util.erl
@@ -1,5 +1,9 @@
 -module(mail_util).
--export([get_arg/1, get_arg/4, strip_path/1, mktemp/1, mktemp/0]).
+-export([get_arg/1, get_arg/4,
+         strip_path/1,
+         mktemp/1, mktemp/0,
+         inject_headers/2,
+         inject_footer/2]).
 
 -include_lib("apptools/include/shorthand.hrl").
 
@@ -65,3 +69,46 @@ mktemp(Dir) ->
 
 mktemp() ->
     ?i2b(erlang:unique_integer([positive])).
+
+%% Exported: inject_headers
+
+inject_headers(Mail, []) ->
+    Mail;
+inject_headers(Mail, NewHeaders) ->
+    case binary:split(Mail, <<"\r\n\r\n">>) of
+        [Headers, Body] ->
+            ?l2b([Headers, <<"\r\n">>,
+                  lists:map(fun({Name, Value}) ->
+                                    [Name, <<": ">>, Value, <<"\r\n">>]
+                            end, NewHeaders),
+                  <<"\r\n">>,
+                  Body]);
+        _ ->
+            Mail
+    end.
+
+%% Exported: inject_footer
+
+inject_footer(Mail, Footer) ->
+    [Headers, Body] = binary:split(Mail, <<"\r\n\r\n">>),
+    case is_plain_text(Headers) of
+        yes ->
+            ?l2b([Headers, <<"\r\n\r\n">>,
+                  string:trim(Body, trailing),
+                  Footer, <<"\r\n">>]);
+        no ->
+            Mail
+    end.
+
+is_plain_text(Headers) ->
+    case binary:match(Headers, <<"Content-Type: text/plain">>) of
+        {_Start, _Length} ->
+            yes;
+        nomatch ->
+            case binary:match(Headers, <<"Content-Type:">>) of
+                nomatch ->
+                    yes;
+                _ ->
+                    no
+            end
+    end.
